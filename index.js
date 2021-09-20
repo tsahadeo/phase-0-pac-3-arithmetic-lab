8d40585792905eab1ac0317577b3d0626572864a
Travis <tsahadeo@mac.com>
2021-09-19 20:06:59 -0400

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,48 @@
+var a= 1,b =80
+function add(a, b) {  
+}
+
+function subtract(a, b){
+}
+
+function multiply(a, b){
+}
+
+function divide(a, b){
+}
+
+function divide(a, b){
+}
+
+function add(a, b){
+    return a + b
+}
+
+function subtract(b, a){
+    return b - a
+}
+
+function multiply(a, b){
+    return a * b
+}
+
+function divide(b, a){
+    return b / a
+}
+
+let n = 1
+function increment(n){
+    return ++ n
+}
+
+function decrement(n){
+    return -- n
+}
+
+function makeInt(n) {
+    return parseInt(n,10);
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n)
+}
